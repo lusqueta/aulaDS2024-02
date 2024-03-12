e86e0a0b03917b5fd0ca21a2d8120dc09002d0f7
LUCAS_3941 <lucas.araujo.pereira23@gmail.com>
2024-03-12 14:39:42 -0300

    added EntradaComScanner.java

---
+++ src/EntradaComScanner.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class EntradaComScanner {
+    public static void main(String[] args) {
+        float largura, comprimento, area, perimetro;
+        Scanner sc;
+        try {
+            System.out.println("Entre com o comprimento: ");
+            sc = new Scanner(System.in);
+            largura = sc.nextFloat();
+        }
+        catch {
+
+        }
+    }
+}
